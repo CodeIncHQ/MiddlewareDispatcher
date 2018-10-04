6207263b303c532b56dd466bc39a0aaa5468ecfa
Joan Fabrégat <joan@codeinc.fr>
2018-10-04 10:07:50 +0200

    Adds support for a customizable final request handler

--- src/DefaultFinalRequestHandler.php
+++ src/DefaultFinalRequestHandler.php
@@ -3,34 +3,44 @@
 // +---------------------------------------------------------------------+
 // | CODE INC. SOURCE CODE                                               |
 // +---------------------------------------------------------------------+
-// | Copyright (c) 2017 - Code Inc. SAS - All Rights Reserved.           |
+// | Copyright (c) 2018 - Code Inc. SAS - All Rights Reserved.           |
 // | Visit https://www.codeinc.fr for more information about licensing.  |
 // +---------------------------------------------------------------------+
 // | NOTICE:  All information contained herein is, and remains the       |
 // | property of Code Inc. SAS. The intellectual and technical concepts  |
 // | contained herein are proprietary to Code Inc. SAS are protected by  |
 // | trade secret or copyright law. Dissemination of this information or |
-// | reproduction of this material  is strictly forbidden unless prior   |
+// | reproduction of this material is strictly forbidden unless prior    |
 // | written permission is obtained from Code Inc. SAS.                  |
 // +---------------------------------------------------------------------+
 //
 // Author:   Joan Fabrégat <joan@codeinc.fr>
-// Date:     11/04/2018
-// Time:     18:40
+// Date:     04/10/2018
 // Project:  MiddlewareDispatcher
 //
 declare(strict_types=1);
 namespace CodeInc\MiddlewareDispatcher;
 use CodeInc\Psr7Responses\NotFoundResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 
 /**
- * Class NoResponseAvailable
+ * Class DefaultFinalRequestHandler
  *
  * @package CodeInc\MiddlewareDispatcher
  * @author Joan Fabrégat <joan@codeinc.fr>
  */
-class NoResponseAvailable extends NotFoundResponse
+final class DefaultFinalRequestHandler implements RequestHandlerInterface
 {
-
+    /**
+     * @inheritdoc
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request):ResponseInterface
+    {
+        return new NotFoundResponse();
+    }
 }
